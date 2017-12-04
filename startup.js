4d2677fadd2361505a48939626e5e0ba98f45345
Dorian <dorianlods@gmail.com>
2017-12-04 23:27:03 +0100

    update

--- startup.js
+++ startup.js
@@ -104,7 +104,7 @@ function createServer(){
             if (!exist) {
                 // if the file is not found, return 404
                 res.statusCode = 404;
-                res.end(`File ${pathname} not found!`);
+                res.end("File " + pathname +" not found!");
                 return;
             }
 
@@ -115,7 +115,7 @@ function createServer(){
             fs.readFile(pathname, function (err, data) {
                 if (err) {
                     res.statusCode = 500;
-                    res.end(`Error getting the file: ${err}.`);
+                    res.end('Error getting the file: ' + err);
                 } else {
                     // if the file is found, set Content-type and send data
                     res.setHeader('Content-type', map[ext] || 'text/plain');
